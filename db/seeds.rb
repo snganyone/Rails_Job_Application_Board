a9fc4c39fa145b8660afa05570e76ae339fa725b
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-09-27 22:38:38 -0700

    DB:Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: "Test", password: "test")
+Agency.create(name: "Atlas", bio: "Looking for talent", agency_type: "Staffing")
+
+Job.create(title: "Software Architect", employer: "Google", location: "San Francisco, CA", description: "Write code", release_date: Date.new, job_type: "Full Time", user_id: 1, agency_id: 1)
+Job.create(title: "Project Manager", employer: "IBM", location: "Seattle, WA", description: "Write code", release_date: Date.new, job_type: "Full Time", user_id: 1, agency_id: 1)
+Job.create(title: "Front End Developer", employer: "Nintendo", location: "New York, NY", description: "Write code", release_date: Date.new, job_type: "Full Time", user_id: 1, agency_id: 1)
+Job.create(title: "Full Stack Developer", employer: "Oracle", location: "Miami, FL", description: "Write code", release_date: Date.new, job_type: "Full Time", user_id: 1, agency_id: 1)
+Job.create(title: "Back End Developer", employer: "Microsoft", location: "Atlanta, GA", description: "Write code", release_date: Date.new, job_type: "Full Time", user_id: 1, agency_id: 1)
+
