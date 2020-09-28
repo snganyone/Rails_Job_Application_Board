b2ff55849423537d27a3a6d4b4d7d4c13f4b58d4
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-09-27 19:34:30 -0700

    Application Schema

---
+++ db/migrate/20200928022517_create_jobs.rb
@@ -0,0 +1,15 @@
+class CreateJobs < ActiveRecord::Migration[6.0]
+  def change
+    create_table :jobs do |t|
+      t.string :title
+      t.string :employer
+      t.string :location
+      t.string :description
+      t.date :release_date
+      t.string :job_type
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
