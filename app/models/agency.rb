70af23e63360a5bd4a851f749b9c54366b8ce89b
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-09-27 19:42:51 -0700

    Password Digest

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -1,4 +1,6 @@
 class Agency < ApplicationRecord
     has_many :jobs
     has_many :users, through: :jobs
+
+    has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_28_023224) do
+ActiveRecord::Schema.define(version: 2020_09_28_024209) do
 
   create_table "agencies", force: :cascade do |t|
     t.string "name"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2020_09_28_023224) do
     t.text "bio"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
