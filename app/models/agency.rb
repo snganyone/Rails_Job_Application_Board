7b61e8415ac8a3abecd64dc7e3ad3f91957b1359
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-09-27 19:43:23 -0700

    Password Digest

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -1,6 +1,4 @@
 class Agency < ApplicationRecord
     has_many :jobs
     has_many :users, through: :jobs
-
-    has_secure_password
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
     has_many :jobs
     has_many :jobs, through: :agencies
+    has_secure_password
 end
