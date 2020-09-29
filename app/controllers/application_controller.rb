d872f14ed81d10d7bd5aeaf101fffeb139704a17
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-09-28 20:25:38 -0700

    Current User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-    helper_method :logged_in?, :current_user
-    
+    helper_method :logged_in?, :current_user, logged_in_user
+
     def home
     end
 
@@ -16,5 +16,10 @@ class ApplicationController < ActionController::Base
 
     def logged_in?
         !!current_user
+    end
+
+    def logged_in_user
+        if params[:id] == session[:user_id]
+            @correct_user ||= User.find_by(session[:id]) id session[:id]
     end
 end
