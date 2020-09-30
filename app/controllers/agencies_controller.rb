2c8fa9c6054b185cf852bb1549471135136cf0a8
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-09-30 16:53:48 -0700

    Add Job Agency

--- app/controllers/agencies_controller.rb
+++ app/controllers/agencies_controller.rb
@@ -6,4 +6,19 @@ class AgenciesController < ApplicationController
 	def new
 		@agency = Agency.new
 	end
+
+	def create
+		@agency = Agency.new(agency_params)
+		if @agency.valid?
+			flash[:alert] = "Agency Successfully Created!"
+			@agency.save
+			redirect_to agency_jobs_path	
+		end
+	end
+
+	private 
+
+	def agency_params
+		params.require(:agency).permit(:name, :bio, :agency_type)
+	end
 end
