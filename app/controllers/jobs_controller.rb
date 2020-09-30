512398d776c57e9c973a8324bf3e701b6b14e8f8
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-09-30 16:20:20 -0700

    Landing Page

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -10,8 +10,8 @@ class JobsController < ApplicationController
 		if current_user.admin == true
 			@job = Job.new
 		else
-			flash[:alert] = "Your are not admin, sorry"
-			redirect_to jobs_path
+			flash[:alert] = "Your are not admin, only system administrators can add job postings."
+			redirect_to root_path
 		end
 	end
 	
