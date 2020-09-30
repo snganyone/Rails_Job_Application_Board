7248f3a0c6346cda94b0e7f6d4fd385c4396fd47
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-09-29 19:17:20 -0700

    User Experience

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,5 @@
 class Job < ApplicationRecord
     belongs_to :user
     belongs_to :agency
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
     has_many :agencies, through: :jobs
 
     has_secure_password
+    
+    #Validations
+    validates :name, presence: true
+    validates :password, presence: true
 end
