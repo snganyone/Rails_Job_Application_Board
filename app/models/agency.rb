d5fc27bf2ed8d4c8537aab6f89cef221fa5b01be
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-09-29 19:19:26 -0700

    Validations

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -1,4 +1,9 @@
 class Agency < ApplicationRecord
     has_many :jobs
     has_many :users, through: :jobs
+
+    #Validations
+    validates :name, presence: true
+    validates :bio, length: { maximum: 500 }
+
 end
