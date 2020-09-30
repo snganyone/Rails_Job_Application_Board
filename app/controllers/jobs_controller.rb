f7c92c735095b81a74636044b76ede0d4df7a409
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-09-29 20:47:33 -0700

    User Admin

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -7,12 +7,16 @@ class JobsController < ApplicationController
 	end
 
 	def new
-		@job = Job.new
+		if current_user.admin == true
+			@job = Job.new
+		else
+			flash[:alert] = "Your are not admin, sorry"
+			redirect_to jobs_path
+		end
 	end
 	
 	def create
 		@job = Job.new(job_params)
-		
 		if @job.valid?
 			flash[:alert] = "Job Successfully Created!"
 			@job.save
@@ -25,7 +29,11 @@ class JobsController < ApplicationController
 	end
 
 	def edit
-		@job = Job.find(params[:id])
+		if current_user.admin == true
+			@job = Job.find(params[:id])
+		else
+			redirect_to jobs_path
+		end
 	end
 
 	def update
@@ -36,9 +44,13 @@ class JobsController < ApplicationController
 
 	#Delete Action
 	def destroy
-		@job = Job.find_by_id(params[:id])
-		@job.destroy
-		redirect_to jobs_path
+		if current_user.admin == true
+			@job = Job.find_by_id(params[:id])
+			@job.destroy
+			redirect_to jobs_path
+		else
+			redirect_to jobs_path
+		end
 	end
 
 	private
