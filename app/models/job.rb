306ae7f30e856bc980a1e3f93a1e7906e0b4ef3f
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-09-29 19:21:24 -0700

    Validations

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,12 @@
 class Job < ApplicationRecord
     belongs_to :user
     belongs_to :agency
+    
+    validates :title, presence :true
+    validates :employer, presence :true
+    validates :location, presence :true
+    validates :description, length: { maximum: 500 }
+    validates :release_date, presence :true
+    validates :job_type, presence :true
 
 end
